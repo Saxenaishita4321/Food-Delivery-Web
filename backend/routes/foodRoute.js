77e5c49d46e0cd1bbcbf8030ddbd669a32db9e32
Saxenaishita4321 <ishitasaxena261@gmail.com>
2026-01-25 20:29:05 +0530

    Fix backend API routes and multer upload issue

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,16 +1,22 @@
 import express from "express";
 import { addFood, listFood, removeFood } from "../controllers/foodController.js";
 import multer from "multer";
+import fs from "fs";
 
 const foodRouter = express.Router();
 
 // Image storage engine
+if (!fs.existsSync("uploads")) {
+    fs.mkdirSync("uploads");
+} 
 const storage = multer.diskStorage({
-    destination: "uploads",
-    filename: (req, file, cb) => {
-        return cb(null, `${Date.now()}${file.originalname}`)
+    destination: function (req, file, cb) {
+        cb(null, "uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
     }
-})
+});
 
 const upload = multer({storage:storage})
 
